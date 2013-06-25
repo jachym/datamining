d7a4875ee921b0cdac0a77f68d7ed8752eb3d616
Jachym Cepicky <jachym.cepicky@gmail.com>
2013-06-25 23:48:24 +0200

    adding subway symbol fix

--- sql/metro.sql
+++ sql/metro.sql
@@ -1,3 +1,8 @@
+create temp table colors(name char(7), color char(6));
+insert into colors values ('Metro A', '007f00');
+insert into colors values ('Metro B', '827f00');
+insert into colors values ('Metro C', '7f0000');
+
 select point.name,
         ST_Transform(
             ST_Centroid(
@@ -5,13 +10,17 @@ select point.name,
             ),
             4326
         ),
-        'rail-underground' as "marker-symbol"
-    from planet_osm_point as point, 
-         planet_osm_polygon as polygon 
+        'rail-underground' as "marker-symbol",
+        colors.color as "marker-color"
+    from planet_osm_point as point
+    INNER JOIN 
+        colors ON point.name ilike '%'||colors.name,
+     planet_osm_polygon as polygon 
+        --substring(point.name, 1,2) = colors.name
     where polygon.name = 'Praha' AND 
         polygon.admin_level='8' AND 
         point.railway = 'subway_entrance' AND 
         (st_within(point.way, polygon.way)) AND 
         point.name is not null
     GROUP BY
-        point.name
+        point.name, colors.color
